0af3c510be164f751ac5a0a571508f0b82a2d227
clementremillieux <clement.remillieux@gmail.com>
2025-05-01 10:55:38 +0200

    first commit

--- start_trader.sh
+++ start_trader.sh
@@ -4,7 +4,7 @@ if [ -f "$HOME/.zprofile" ]; then
   source "$HOME/.zprofile"
 fi
 
-cd /Users/remillieux/Documents/trader_auto
+cd /home/clement/trader
 
 exec poetry run python3 -m app.trader.trader \
      >> /Users/remillieux/Documents/trader_auto/trader.out.log \
