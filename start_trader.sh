bc9f8e544e814ea500c4375b6b3a2532da091ee2
clementremillieux <clement.remillieux@gmail.com>
2025-05-01 11:27:26 +0200

    first commit

--- start_trader.sh
+++ start_trader.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-# 1. Assurez‐vous que ~/.local/bin (et pyenv si vous l’utilisez) est bien dans le PATH
-export PATH="$HOME/.local/bin:$HOME/.pyenv/shims:$PATH"
 
-# 2. Allez dans le dossier du projet
-cd /home/clement/trader || exit 1
-
-# 3. Récupérez le chemin du venv Poetry
-VENV_PATH=$(/home/clement/.local/bin/poetry env info -p)
+cd /home/clement/trader 
 
 # 4. Lancez le module sur le Python du venv
-exec "$VENV_PATH/bin/python3.10" -m app.trader.trader \
+exec poetry run python3 -m app.trader.trader \
      >> /home/clement/trader/trader.out.log \
      2>> /home/clement/trader/trader.err.log
