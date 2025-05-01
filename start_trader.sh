73f9d3fc1bd316cc0465431f637c4fbf9f3f9504
clementremillieux <clement.remillieux@gmail.com>
2025-05-01 11:01:14 +0200

    first commit

--- start_trader.sh
+++ start_trader.sh
@@ -6,6 +6,6 @@ fi
 
 cd /home/clement/trader
 
-exec poetry run python3 -m app.trader.trader \
+exec /home/clement/.local/bin/poetry run python3 -m app.trader.trader \
      >> /home/clement/trader/trader.out.log \
      2>> /home/clement/trader/trader.err.log
