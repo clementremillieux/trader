ba2a541acab95e059cc2cea27795309c3a1d3463
clementremillieux <clement.remillieux@gmail.com>
2025-05-04 00:41:15 +0200

    move to crypto

--- start_trader.sh
+++ start_trader.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 
-cd /home/clement/trader_cryto 
+cd /home/clement/trader_crypto 
 
 # 4. Lancez le module sur le Python du venv
 exec /home/clement/.local/bin/poetry run python3 -m app.trader.trader \
      >> /home/clement/trader_crypto/trader.out.log \
-     2>> /home/clement/trader_cryto/trader.err.log
+     2>> /home/clement/trader_crypto/trader.err.log
