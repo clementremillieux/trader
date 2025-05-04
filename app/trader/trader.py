16af7510f113b6eb9430922d0680447e4c269561
clementremillieux <clement.remillieux@gmail.com>
2025-05-04 19:14:14 +0200

    move to crypto

--- app/trader/trader.py
+++ app/trader/trader.py
@@ -85,7 +85,7 @@ class Trader:
         ]
 
         self.analysis = Analysis(
-            model_path="app/model/model_epoch_crypto_44.pth",
+            model_path="app/model/model_epoch_crypto_176.pth",
             signals=signals,
             num_historical_features=12,
             encoder_length=self.window_size,
